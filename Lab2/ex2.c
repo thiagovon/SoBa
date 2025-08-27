7abecd849c3613c4d82be807eaaa7951ef22c0f7
Thiago Von <147444190+thiagovon@users.noreply.github.com>
2025-08-27 01:20:58 +0000

    Lab2

--- Lab2/ex2.c
+++ Lab2/ex2.c
@@ -1,21 +1,23 @@
 #include <ctype.h>
 #include <stdio.h>
 
-int string2num (char *s, int t) {
+int string2num (char *s, int base) {
   int a = 0;
-  for (; *s; s++)
-    a = a*10 + (*s - '0');
+  for (; *s; s++) {
+    int digit_value;
+    if (*s >= '0' && *s <= '9') {
+      digit_value = *s - '0';
+    } else {
+      digit_value = *s - 'a' + 10;
+    }
+    a = a * base + digit_value;
+  }
   return a;
 }
 
-/*
-int main (void) {
-  printf("==> %d\n", string2num("1234"));
-  printf("==> %d\n", string2num("1234") + 1);
-  printf("==> %d\n", string2num("1234") + string2num("1"));
+int main(void) {
+  printf("%d\n", string2num("1a", 16));
+  printf("%d\n", string2num("a09b", 16));
+  printf("%d\n", string2num("z09b", 36));
   return 0;
 }
-*/
-
-printf("%d\n", string2num("777", 8));
-printf("%d\n", string2num("777", 10));
