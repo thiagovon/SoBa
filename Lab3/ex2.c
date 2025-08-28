c4766fa39f54183c7513b0007cb799aa4108f7a9
Thiago Von <147444190+thiagovon@users.noreply.github.com>
2025-08-28 13:56:18 +0000

    Commit Lab3

---
+++ Lab3/ex2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int odd_ones(unsigned int x) {
+  /* escreva seu código aqui */
+}
+int main() {
+  printf("%x tem numero %s de bits\n",0x01010101,odd_ones(0x01010101) ? "impar":"par");
+  printf("%x tem numero %s de bits\n",0x01030101,odd_ones(0x01030101) ? "impar":"par");
+  return 0;
+}
