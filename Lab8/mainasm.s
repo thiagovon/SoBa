23a17d0e38d605f0132fae0d54c898a7862931e0
Thiago Von <147444190+thiagovon@users.noreply.github.com>
2025-09-26 21:00:22 +0000

    Lab8

--- Lab8/mainasm.s
+++ Lab8/mainasm.s
@@ -1,52 +1,34 @@
+/*
 #include <stdio.h>
 #define LIM 1
 
-int filtro(int x, int lim);
+int filtro(int x, int lim) {
+  if (x < lim)
+    return 0;
+  else
+    return x;
+}
 
+int nums[5] = {3,-5,7,8,-2};
 
 int main() {
   int i;
   int *p = nums;
-
   for (i = 0; i < 5; i++) {
-    i=0
-
-WHILE:
-#    while (i<5) {
-    cmpl    $5, $ebx
-    jge     FORA_WHILE
-
-#    printf("%d\n", filtro(*p,LIM));  <-- quebrar em 2 linhas mais simples
-#   temp = filtro(*p, LIM)
-    movl    (%r12), %edi    #1o argumento
-    movl    $1, %esi        #2o argumento
-    call    filtro
-
-#   printf("%d\n", filtro);
-    movq    $sf1, %rdi
-    movl    %eax, %esi
-    movl    $0, %eax
-    call    printf
-
-
+    printf("%d\n", filtro(*p,LIM));
     p++;
-    addq    $4, %
-    i++;
   }
-FORA_WHILE:
-#  return 0;
-    movl    $0, %eax
+  return 0;
 }
+*/
 
 
-
-.data # dados inicializados e strings constantes
-#int nums[5] = {3,-5,7,8,-2};
+  .data
   nums: .int 3, -5, 7, 8, -2
   s1:   .string "%d\n"
 
-  .text
-  .globl main
+  .text /* estamos na área de texto */
+  .globl main /* função global main */
   main:
   /* prologo */
      pushq %rbp
@@ -56,9 +38,35 @@ FORA_WHILE:
      movq  %r12, -16(%rbp)
 
   /* coloque seu codigo aqui */
+  movl   $0, %ebx /* i = 0 */
+  movq  $nums, %r12   /* p = nums */
+
+WHILE:
+  /* while (i<5) */
+  cmpl   $5, %ebx 
+  jge FORA_WHILE
+
+  /* temp = filtro(*p, LIM) */
+  /* printf("%d\n", temp) */
+  movl   (%r12), %edi /* primeiro argumento */
+  movl   $1, %esi     /* segundo argumento */
+  call filtro
+
+  movq  $s1, %rdi /* primeiro argumento de printf */
+  movl  %eax, %esi /* segundo argumento de printf */
+  movl  $0, %eax  /* caso especial para printf */
+  call printf
+
+  addq   $4, %r12 /* p++ */
+  incl   %ebx
+  jmp    WHILE
+
+FORA_WHILE:
+  movl   $0, %eax /* return 0 */
 
   /* finalizacao */
      movq -8(%rbp), %rbx
      movq -16(%rbp), %r12
      leave
      ret
+
